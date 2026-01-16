c9ab22044e8c0532db9c7b6485d8eb18bed0e6df
今泉隼人 <imatyann2003@gmail.com>
2026-01-16 22:57:00 +0900

    plus gradlew

--- src/javalanguage/lexer/Lexer.java
+++ src/javalanguage/lexer/Lexer.java
@@ -13,10 +13,56 @@ public final class Lexer {
     
     // Tokenリストを生成する
     public List<Token> tokenize(String source) throws LexError {
-        //　まずEOFだけのリストを生成する
         List<Token> tokens = new ArrayList<>();
         
-        tokens.add(new Token(TokenType.EOF, 0));
+        int l = source.length();
+        int i = 0;
+        
+        // source中の文字に対してそれぞれ実行する
+        while (i < l) {
+            char word = source.charAt(i);
+            // 空白を除去する
+            if (Character.isWhitespace(word)){
+                i++;
+                continue;
+            }
+
+            // 整数リテラルのチェック
+            if (Character.isDigit(word)){
+                // 整数に出会ったら、初めて整数以外に出会うかiがlを超すまで繰り返す
+                int value = 0;
+                int startI = i;
+                while (Character.isDigit(source.charAt(i)) && (i < l) ) {
+                    int n = word - '0';
+                    value = value * 10 + n ;
+                    i ++;
+                }
+
+                tokens.add(new Token(TokenType.INT, startI, value));
+                continue;
+            }
+
+            // 演算子と括弧のチェック
+            switch (word) {
+                case '+':
+                    tokens.add(new Token(TokenType.PLUS, i));
+                case '-':
+                    tokens.add(new Token(TokenType.MINUS, i));
+                case '*':
+                    tokens.add(new Token(TokenType.MUL, i));
+                case '/':
+                    tokens.add(new Token(TokenType.DIV, i));
+                case '(':
+                    tokens.add(new Token(TokenType.LPAREN, i));
+                case ')':
+                    tokens.add(new Token(TokenType.RPAREN, i));
+                default:
+                    // 未知の文字が生じた場合のエラー
+                    throw new LexError(i, "未知の文字：" + word);
+                }
+        }
+
+        tokens.add(new Token(TokenType.EOF, l));
         return tokens;
     }
 
